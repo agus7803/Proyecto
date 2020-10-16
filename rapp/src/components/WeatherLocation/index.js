6e3aa433968d83bb6dde00a6968bc3f80e5d0814
Tincho <javier4ever9@hotmail.com>
2020-10-16 17:22:23 -0300

    LIbrebria para cambio metrico

--- rapp/src/components/WeatherLocation/index.js
+++ rapp/src/components/WeatherLocation/index.js
@@ -11,7 +11,7 @@ const location = "Montevideo,uy";
 const api_key = "4c3db25c23237daf46ae05c3ea6a8343";
 const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}&units=metric`;
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
      temperature: 19,
